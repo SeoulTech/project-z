afb90b2132acbcc3feae1441f3de18f7f681398e
mail@denisu.jp <mail@denisu.jp>
2014-05-21 14:33:17 +0900

    replaced form ids with our new shared doc

--- scripts/main.js
+++ scripts/main.js
@@ -6,15 +6,15 @@ $('#subscribe').submit(function(event) {
   // i use the always function because the post request is failing due to cross scripting browser blocks
   // the data still arrives magically in the google spreadsheet though
 
-  $.post('https://docs.google.com/forms/d/1Nb6UJxA-JS_k-6kU3gjQjtjfhO1z785k6b9msCR_c44/formResponse', {
+  $.post('https://docs.google.com/forms/d/1p4Hk3LKLK6NqPSrxlSXbQdoo-ndSJZ0MmWFpgiHbMNY/formResponse', {
 
-    'entry_1469179455': $('#email').val()
+    'entry.1929687656': $('#email').val()
 
   }).always(function() {
 
     alert('Thanks! We will keep you up to date.');
     $('#subscribe')[0].reset();
 
-  })
+  });
 
 });
